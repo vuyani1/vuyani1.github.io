f9e5ad37bfff07e8206b39650dfacaeb0a941ca0
vuyani1 <vuyaniphila86@gmail.com>
2025-08-25 19:49:36 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://wwcedhbbdoyrmquokyjj.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3Y2VkaGJiZG95cm1xdW9reWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMzYyNjUsImV4cCI6MjA3MTYxMjI2NX0.s29eO4ZQ74R6Jktn5zKZ-AiH6_QYC-_9WFqAS_m1kj4";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
